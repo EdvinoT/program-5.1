b282e4603e1ee7de5a51f8835a3603ce092e5075
EdvinoT <edvinot82@gmail.com>
2025-11-06 14:20:47 +0000

    commit

---
+++ main.py
@@ -0,0 +1,29 @@
+#Program Calucate sales commsions
+def main():
+    keep_going == "y"
+    
+    #Calculate deries of commision
+    
+    while keep_going == "y":
+        sales = input("Enter the amount of sales: ")
+        comm_rate = input("Enter the amount of commission rate: ")
+        commission = sales* comm_rate
+        #Display
+        print("the commission is $%.2." %commission) 
+        #see if user wantes another 
+        keep_going = raw-input('Do you want to calcualte another' + /)
+
+
+
+
+#names=["Tommy","David","Valdez","Zagada","Edvino"]
+#for tdvze in names:
+    #print("my name is " + tdvze)
+    #print("")
+
+#Conditional Controlled 
+#condition=True
+#while(condition):
+    #print(1+2)
+    #print()
+    #condition=False
